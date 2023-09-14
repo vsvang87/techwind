784ba8b1fbceb07515b99881ee1d50ab7a677b77
Visay Vang <vsvang@gmail.com>
2023-09-14 19:22:31 -0400

    add flex, color, fonts, form

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import seo from "/images/seo.svg";
+import { Link } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
+import img from "/images/heart-phone.svg";
 
 const Section1 = () => {
   return (
@@ -24,6 +27,48 @@ const Section1 = () => {
                 you can take a component from the Application theme and use it
                 in the Website.
               </p>
+              <ul>
+                <li>Digital Marketing Solutions for Tomorrow</li>
+                <li>Our Talented & Experienced Marketing Agency</li>
+                <li>Create your won skin to match your brand</li>
+              </ul>
+            </div>
+          </div>
+          <div className="content-3">
+            <div className="content-3-left">
+              <h1>Speed up your development with Techwind.</h1>
+              <p>
+                You can combine all the Techwind templates into a single one,
+                you can take a component from the Application theme and use it
+                in the Website.
+              </p>
+              <ul>
+                <li>Digital Marketing Solutions for Tomorrow</li>
+                <li>Our Talented & Experienced Marketing Agency</li>
+                <li>Create your own skin to match your brand</li>
+              </ul>
+              <Link to="">
+                Find Out More <FiChevronRight />
+              </Link>
+            </div>
+            <div className="content-3-right">
+              <div className="form">
+                <img src={img} alt="phone-heart img" />
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="text" />
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
