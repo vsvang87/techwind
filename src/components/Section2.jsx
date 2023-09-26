65e5692f5b534f6b1b73a3bb8ad20f84c746a0e0
Visay Vang <vsvang@gmail.com>
2023-09-26 12:15:51 -0400

    add img, padding, margin

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,17 +1,48 @@
 import React from "react";
+import img1 from "/images/thomas.webp";
+import img2 from "/images/carl.webp";
 
 const Section2 = () => {
   return (
     <section className="section-2">
       <div className="container">
         <div className="sect-2-content">
-          <p>We believe in building each other and hence</p>
-          <h1>Work with some amazing partners</h1>
-          <p>
-            Start working with Tailwind CSS that can provide everything you need
-            to generate awareness, drive traffic, connect.
-          </p>
-          <div className="carousel-div"></div>
+          <div className="content-1">
+            <p className="blue-text">
+              We believe in building each other and hence
+            </p>
+            <h1>Work with some amazing partners</h1>
+            <p>
+              Start working with Tailwind CSS that can provide everything you
+              need to generate awareness, drive traffic, connect.
+            </p>
+          </div>
+          <div className="carousel-div">
+            <div className="carousel-card">
+              <img src={img1} alt="" />
+              <div className="carousel-text">
+                <p>
+                  It seems that only fragments of the original text remain in
+                  the tech texts are used today.
+                </p>
+                <p>Thomas Israel</p>
+                <p>Staff Engineer</p>
+              </div>
+            </div>
+            <div className="carousel-card">
+              <img src={img2} alt="" />
+              <div className="carousel-text">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+                  vel laborum vitae dolore? Sint ducimus dignissimos libero.
+                  Nihil, qui nulla aspernatur debitis incidunt dicta voluptates
+                  deserunt perspiciatis dolorem est quod.
+                </p>
+                <p>Jill Valentine</p>
+                <p>Staff Engineer</p>
+              </div>
+            </div>
+          </div>
 
           <div className="rates-div">
             <div className="rates-left">
